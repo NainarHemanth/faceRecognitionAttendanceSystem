e78496b21ef29cc185bdf06604716fd7aa614ca4
Nainar  Hemanth Kumar Sai <108261774+NainarHemanth@users.noreply.github.com>
2024-11-06 14:55:28 +0530

    Update AddData2Database.py

--- AddData2Database.py
+++ AddData2Database.py
@@ -2,14 +2,25 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred,{
-    'databaseURL': "https://realtimefaceattendance-e49f5-default-rtdb.firebaseio.com/"
+cred = credentials.Certificate("ServiceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://facerecognition-9e552-default-rtdb.firebaseio.com/"
 })
 
 ref = db.reference('Students')
 
 data = {
+    "RA2111003011294":
+        {
+            "Name": "N Hemanth Kumar Sai",
+            "id": "RA2111003011294",
+            "Department": "CTECH",
+            "Section": "E2",
+            "Batch": 2,
+            "Academic Year": "2021 - 2025",
+            "Total-attendance": 40,
+            "Last-attendance-time": "2023-12-11 11:59:00"
+        },
     "RA2111026010179":
         {
             "Name": "Uday Kiran C",
@@ -54,19 +65,19 @@ data = {
             "Total-attendance": 30,
             "Last-attendance-time": "2023-10-13 13:59:00"
         },
-"RA2111026010038":
+    "RA2111003011611":
         {
-            "Name": "Yashwanth Marni",
-            "id": "RA2111026010038",
-            "Department": "CINTEL",
-            "Section": "Y1",
-            "Batch": 1,
+            "Name": "Krishna Chaitanya",
+            "id": "RA2111003011611",
+            "Department": "CTECH",
+            "Section": "J2",
+            "Batch": 2,
             "Academic Year": "2021 - 2025",
             "Total-attendance": 38,
             "Last-attendance-time": "2023-10-13 13:59:00"
         },
 }
+# sending data, unzipping dictnaries in python
 
-#sending data, unzipping dictnaries in python
 for key, value in data.items():
     ref.child(key).set(value)
